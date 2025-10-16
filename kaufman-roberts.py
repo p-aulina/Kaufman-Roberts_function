a420c460be70c39e8e8fc1d9aa7018c4914ab08f
Paulina Górska <p.gorska2004@gmail.com>
2025-10-16 19:08:57 +0200

    probability of occupancy

--- kaufman-roberts.py
+++ kaufman-roberts.py
@@ -16,21 +16,40 @@ def calculate_a():
         x += a_step
     # print(step_counter)
 
-    for i in range(m):
-        x = C/(m*t[i])
-        for j in range(len(a)):
+    for j in range(len(a)):
+        for i in range(m):
+            x = C/(m*t[i])
+        
             ai.append(x*a[j])
 
 s = []
 p = []
+x = 0
 
-# def calculate_p():
-#     s.append(1)
-#     x = 0
-#     for i in range(m):
-#         for n in range(len(ai)):
-#             if (n - t[i] >= 0):
-#                 x +=
+def calculate_p():
+    for i in range(0, len(ai), m):
+        s.append(1)
+        l = len(s) - 1
+        for n in range(1, C + 1):
+            x = 0
+            for j in range(m):
+                if(n - t[j] >= 0):
+                    x += ai[i + j]*t[j]*s[n - t[j] + l]
+            s.append(x/n)
+
+    for i in range(0, len(s), C + 1):
+        sum = 0
+        for j in range(C + 1):
+            sum += s[i + j]
+        for j in range(C + 1):
+            p.append(s[j]/sum)
+
+
+
+
+    # for i in range(len(s)):
+    #     print(s[i])
+
 
 
 
@@ -42,7 +61,17 @@ def print_ai():
     for i in range(x):
         print(ai[i], ai[x + i])
 
+def print_ai2():
+    for i in range(0, len(ai)//2, 2):
+        print(ai[i], ai[i+1])
+def print_s():
+    for i in range(len(s)):
+        if (i%5 == 0):
+            print("i", i//5)
+        print(s[i])
+
 
 
 calculate_a()
-print_ai()
+print_ai2()
+calculate_p()
