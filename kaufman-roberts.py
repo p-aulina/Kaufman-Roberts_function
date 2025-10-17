eee8aa490b34ff27ba2897d2868d322d2dd5f4aa
Paulina Górska <p.gorska2004@gmail.com>
2025-10-17 13:21:52 +0200

    comments

--- kaufman-roberts.py
+++ kaufman-roberts.py
@@ -1,3 +1,13 @@
+# ******************************************************************
+#                        [ INPUT PARAMETERS ]
+#
+#  a_min - minimum traffic offered per one unit of system capacity
+#  a_max - maximum traffic offered per one unit of system capacity
+#  a_step - calculation step
+#  C - system capacity
+#  t - stream requests
+#  m - class number
+# ******************************************************************
 a_min = 0.2
 a_max = 1.3
 a_step = 0.1
@@ -5,16 +15,22 @@ C = 4
 t = [1, 2]
 m = len(t)
 
-ai = []
 
+# ******************************************************************
+#                       [ TRAFFIC INTENSITY ]
+# ******************************************************************
+ai = []
 def calculate_a():
     a = a_min*C/m
     for i in range(0, m, 1):
         ai.append(a/t[i])
 
+
+# ******************************************************************
+#              [ OCCUPANCY PROBABILITY DISTRIBUTION ]
+# ******************************************************************
 s = []
 p = []
-
 def calculate_p():
     s.append(1)
     for n in range(1, C + 1, 1):
@@ -29,6 +45,10 @@ def calculate_p():
     for n in range(0, C + 1, 1):
         p.append(s[n]/sum)
 
+
+# ******************************************************************
+#                      [ BLOCKING PROBABILITY ]
+# ******************************************************************
 ei = []
 def calculate_e():
     for i in range(m):
@@ -37,44 +57,47 @@ def calculate_e():
             x += p[n]
         ei.append(x)
 
+
+# ******************************************************************
+#                        [ HELPER FUNCTIONS ]
+# ******************************************************************
 def calculate_all():
     calculate_a()
     calculate_p()
     calculate_e()
-
-def print_all():
-    print(a_min, end="\t")
-    for i in range(m):
-        print(ei[i], end = "\t")
-    print("")
-        
 
 def clear_all():
     ai.clear()
     s.clear()
     p.clear()
     ei.clear()
-    
 
 def header():
     print("#", "C =", C)
     print("#")
     for i in range(m):
-        print("#      ", "t[%i] =" % (i + 1), t[i])
+        print("# \t", "t[%i] =" % (i + 1), t[i])
     print("#")
-    print("#    ", end="")
+    print("# \t", end="")
     for i in range(m - 1):
-        print("t%i" % (i + 1), "      ", end="")
+        print("t%i" % (i + 1), "\t \t \t", end="")
     
     print("t%i" % m)
 
+def print_all():
+    print(a_min, end="\t")
+    for i in range(m):
+        print(ei[i], end = "\t")
+    print("")
 
+
+# ******************************************************************
+#                              [ MAIN ]
+# ******************************************************************
+header()
 while(a_min <= a_max):
     calculate_all()
     print_all()
     clear_all()
     a_min += a_step
     a_min = round(a_min, 1)
-
-
-# header()
